04e0022034b9751829a5a35da507f75fab1fbd4e
yumi <aym7330@naver.com>
2018-12-23 15:59:26 +0900

    binarySearch메서드 commit

--- algorithm/src/chap03/Q6.java
+++ algorithm/src/chap03/Q6.java
@@ -1,19 +1,15 @@
 /**
  * 날짜 : 2018-12-23
  * 
- * 페이지 : p120
- * 
- * binarySearch 메서드
- * 장점1 : 이진 검색 메서드를 직접 코딩할 필요가 없다.
- * 장점2 : 모든 자료형 배열에서 검색할 수 있다.
- * 검색에 성공한 경우  : key와 일치하는 요소의 인덱스를 반환, 일치하는 요소가 여러개 있다면 무작위의 인덱스를 반환
- * 검색에 실패한 경우 : 반환값은 삽입 포인트를 x라고 할 떄 -x-1을 반환, 삽입포인트는 검색하기 위해 지정한 key보다 큰요소 중 첫번째 요소의 인덱스
+ * 페이지 : p121
  */
+
 package chap03;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Test3_5 {
+public class Q6 {
 	public static void main(String[] args) {
 		Scanner stdIn=new Scanner(System.in);
 		
@@ -38,10 +34,19 @@ public class Test3_5 {
 		
 		int idx=Arrays.binarySearch(x, ky);
 		
-		if(idx==-1)
-			System.out.println("그 값의 요소가 없습니다.");
+		if(idx<=-1) {
+			idx=idx+1;
+			idx=-idx;
+			System.out.println("삽입포인트는 +"+idx+"입니다.");
+		}
 		else
 			System.out.println(ky+"은(는) x["+idx+"]에 있습니다.");
+/*		if (idx < 0) {
+			int insPoint = -idx - 1;
+			System.out.println("그 값의 요소가 없습니다.");
+			System.out.printf("삽입 포인트는 %d입니다.\n", insPoint);
+			System.out.printf("x[%d]의 바로 앞에 %d를 삽입하면 배열의 정렬 상태가 유지됩니다.", insPoint, ky);
+		} else
+			System.out.println("그 값은 x[" + idx + "]에 있습니다.");*/
 	}
-
 }
