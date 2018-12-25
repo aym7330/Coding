e1847999bffa893ed8732d99364f55b093e8feec
yumi <aym7330@naver.com>
2018-12-25 15:23:50 +0900

    stack commit

---
+++ algorithm/src/chap04/Test4_1.java
@@ -0,0 +1,105 @@
+/**
+ * 날짜 : 2018-12-24
+ * 
+ * 페이지 : p136
+ * 
+ * 스택 : 데이터를 일시적으로 저장하기 위한 자료구조, 데이터의 입력과 출력 순서는 후입선출(LIFO)
+ * 푸시(push) : 데이터를 넣는 작업 
+ * 팝(pop) : 데이터를 꺼내는 작업
+ * => Java 프로그램에서 메서드를 호출하고 실행할 때 프로그램 내부에서는 스택을 사용
+ * 
+ */
+
+package chap04;
+
+public class Test4_1 {
+	private int max;	//스택 용량
+	private int ptr;	//스택 포인터
+	private int[] stk;	//스택 본체
+	
+	// 실행 시 예외 : 스택이 비어 있음
+	public class EmptyIntStackException extends RuntimeException{
+		public EmptyIntStackException() {}
+	}
+	
+	// 실행 시 예외 : 스택이 가득 참
+	public class OverflowIntStackException extends RuntimeException{
+		public OverflowIntStackException() {}
+	}
+	
+	// 생성자 : 스택 본체용 배열을 생성하는 등 준비작업을 수행
+	public Test4_1(int capacity) {
+		ptr=0;
+		max=capacity;
+		try {
+			stk=new int[max];			// 스택 본체용 배열 생성
+		}catch (OutOfMemoryError e) {	// 생성할 수 없음
+			max=0;
+		}
+	}
+	
+	// 스택에 x를 푸시
+	public int push(int x) throws OverflowIntStackException{
+		if(ptr>=max)	//스택이 가득 참
+			throw new OverflowIntStackException();
+		return stk[ptr++]=x;
+	}
+	
+	// 스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄)
+	public int pop() throws EmptyIntStackException{
+		if(ptr<=0)
+			throw new EmptyIntStackException();
+		return stk[--ptr];
+	}
+	
+	//스택에서 데이터를 피크(정상에 있는 데이터를 들여다 봄)
+	public int peek() throws EmptyIntStackException{
+		if(ptr<=0)
+			throw new EmptyIntStackException();
+		return stk[ptr-1];
+	}
+	
+	// 스택에서 x를 찾아 인덱스(없으면 -1)를 반환
+	public int indexOf(int x) {
+		for(int i=ptr-1;i>=0;i--)	// 정상 쪽에서 선형 검색
+			if(stk[i]==x)
+				return i;
+		return -1;
+	}
+	
+	// 스택을 비움
+	public void clear() {
+		ptr=0;
+	}
+	
+	// 스택의 용량을 반환
+	public int capacity() {
+		return max;
+	}
+	
+	// 스택에 쌓여 있는 데이터 수를 반환
+	public int size() {
+		return ptr;
+	}
+	
+	// 스택이 비어 있는가?
+	public boolean isEmpty() {
+		return ptr<=0;
+	}
+	
+	// 스택이 가득 찼는가?
+	public boolean isFull() {
+		return ptr>=max;
+	}
+	
+	// 스택 안의 모든 데이터를 바닥 -> 꼬대기 순서로 출력
+	public void dump() {
+		if(ptr<=0)
+			System.out.println("스택이 비어 있습니다.");
+		else {
+			for(int i=0;i<ptr;i++)
+				System.out.println(stk[i]+" ");
+			System.out.println();
+		}
+	}
+}
---
+++ algorithm/src/chap04/Test4_2.java
@@ -0,0 +1,11 @@
+/**
+ * 날짜 : 2018-12-24
+ * 
+ * 페이지 : p129
+ */
+
+package chap04;
+
+public class Test4_2 {
+
+}
